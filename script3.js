96ef0ad329e4384eb766ec3ad6583069eb7dd345
desilus <164355076+desilus@users.noreply.github.com>
2024-06-26 15:07:12 +0200

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,142 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const domande = [
+        { domanda: "RECATI SUBITO ALLA TAPPA NUMERO 1. Benvenuti in ALBANIA! Per superare questa tappa e fare tanti punti segui attentamente le indicazioni dello staff!", rispostaCorretta: "spaghetti", immagine: "immagini/albania.svg" },
+        { domanda: "VAI ALLA TAPPA 2! Benvenuti in BOSNIA! Per superare questa tappa e fare tanti punti segui attentamente le indicazioni dello staff!", rispostaCorretta: "pizza", immagine: "immagini/bosnia.svg" },
+        { domanda: "VOLA ALLA TAPPA 3! Benvenuti in BULGARIA! Ascolta bene lo staff!", rispostaCorretta: "lasagne", immagine: "immagini/bulgaria.svg" },
+        { domanda: "VAI ALLA TAPPA 4! Benvenuti in GHANA! Sui tavolini troverai un cruciverba. Una volto risolto avrai la parola d’ordine!", rispostaCorretta: "casoncelli", immagine: "immagini/ghana.svg" },
+        { domanda: "VAI ALLA TAPPA 5. Benvenuti in INDIA! Ascolta bene lo staff!", rispostaCorretta: "gnocchi", immagine: "immagini/india.svg" },
+        { domanda: "VAI ALLA TAPPA 6. Benvenuti in ITALIA, leggi le istruzioni attentamente e avrai la parola d’ordine. Insieme alla squadra create un video e mandatecelo al seguente indirizzo test@testone.testina", rispostaCorretta: "pappardelle", immagine: "immagini/italia.svg" },
+        { domanda: "VIA LIBERA PER LA TAPPA 7! Benvenuti in MACEDONIA! Entra in cantina ed ascolta lo staff!", rispostaCorretta: "gelato", immagine: "immagini/macedonia.svg" },
+        { domanda: "VAI ALLA TAPPA 8! Benvenuti in MAROCCO. Leggete le istruzioni attentamente per la parola d’ordine!", rispostaCorretta: "tortellini", immagine: "immagini/marocco.svg" }
+    ];
+
+    const squadra = parseInt(document.body.getAttribute('data-squadra'), 9);
+    const domandeContainer = document.getElementById("domande-container");
+    const timbroContainer = document.getElementById("timbro-container");
+    const progressBar = document.getElementById("progress-bar");
+
+    function getDomandePerSquadra(domande, squadra) {
+        const start = (squadra - 1) % domande.length;
+        const orderedQuestions = [];
+
+        for (let i = 0; i < domande.length; i++) {
+            orderedQuestions.push(domande[(start + i) % domande.length]);
+        }
+
+        return orderedQuestions;
+    }
+
+    const domandePerSquadra = getDomandePerSquadra(domande, squadra);
+    let currentIndex = localStorage.getItem('currentIndex') ? parseInt(localStorage.getItem('currentIndex')) : 0;
+    let risposteCorrette = localStorage.getItem('risposteCorrette') ? parseInt(localStorage.getItem('risposteCorrette')) : 0;
+
+    domandePerSquadra.forEach((domandaObj, index) => {
+        const div = document.createElement("div");
+        div.className = "domanda";
+        div.innerHTML = `
+            <label for="domanda${index + 1}">${domandaObj.domanda}</label>
+            <div class="cici"></div>
+            <p>PAROLA D'ORDINE</p>
+            <input type="text" class="domandaDinamica" id="domanda${index + 1}" name="domanda${index + 1}">
+        `;
+        domandeContainer.insertBefore(div, document.getElementById("pagina-commiato"));
+    });
+
+    function aggiornaProgressBar() {
+        const progressPercent = (risposteCorrette / domandePerSquadra.length) * 100;
+        progressBar.style.width = `${progressPercent}%`;
+    }
+
+    function mostraProssimaDomanda() {
+        const activeDiv = document.querySelector(".domanda.active, .pagina.active");
+        const isPagina = activeDiv.classList.contains("pagina");
+
+        if (isPagina) {
+            activeDiv.classList.remove("active");
+            currentIndex++;
+            localStorage.setItem('currentIndex', currentIndex);
+            const nextDiv = document.querySelectorAll(".domanda, .pagina")[currentIndex];
+            nextDiv.classList.add("active");
+            if (nextDiv.classList.contains("domanda")) {
+                timbroContainer.innerHTML = `<img src="${domandePerSquadra[currentIndex - 1].immagine}" alt="Immagine Domanda" class="bnw">`;
+            } else {
+                timbroContainer.innerHTML = '';
+            }
+        } else {
+            const input = activeDiv.querySelector("input");
+            const risposta = input.value.trim();
+            const rispostaCorretta = domandePerSquadra[currentIndex - 1].rispostaCorretta;
+            const immagine = domandePerSquadra[currentIndex - 1].immagine;
+
+            if (risposta.toLowerCase() === rispostaCorretta.toLowerCase()) {
+                const imgElement = timbroContainer.querySelector('img');
+                imgElement.classList.remove('bnw');
+                imgElement.classList.add('color');
+
+                const timbro = document.createElement('div');
+                timbro.className = 'timbro';
+                timbroContainer.appendChild(timbro);
+
+                timbro.style.display = 'block';
+
+                risposteCorrette++;
+                localStorage.setItem('risposteCorrette', risposteCorrette);
+                aggiornaProgressBar();
+
+                setTimeout(() => {
+                    timbro.style.display = 'none';
+                    activeDiv.classList.remove("active");
+                    currentIndex++;
+                    localStorage.setItem('currentIndex', currentIndex);
+                    const nextDiv = document.querySelectorAll(".domanda, .pagina")[currentIndex];
+                    nextDiv.classList.add("active");
+                    if (nextDiv.classList.contains("domanda")) {
+                        nextDiv.querySelector("input").focus();
+                        timbroContainer.innerHTML = `<img src="${domandePerSquadra[currentIndex - 1].immagine}" alt="Immagine Domanda" class="bnw">`;
+                    } else {
+                        timbroContainer.innerHTML = '';
+                        document.getElementById("next-button").style.display = "none";
+                        localStorage.removeItem('currentIndex');
+                        localStorage.removeItem('risposteCorrette');
+                    }
+                }, 1000);
+            } else {
+                alert("Risposta errata. Riprova.");
+            }
+        }
+    }
+
+    document.getElementById("next-button").addEventListener("click", mostraProssimaDomanda);
+
+    document.querySelectorAll(".domanda input").forEach(input => {
+        input.addEventListener("keypress", function(event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                mostraProssimaDomanda();
+            }
+        });
+    });
+
+    function mostraDomandaCorrente() {
+        const divs = document.querySelectorAll(".domanda, .pagina");
+        divs.forEach(div => div.classList.remove("active"));
+
+        const activeDiv = divs[currentIndex];
+        activeDiv.classList.add("active");
+
+        if (activeDiv.classList.contains("domanda")) {
+            activeDiv.querySelector("input").focus();
+            timbroContainer.innerHTML = `<img src="${domandePerSquadra[currentIndex - 1].immagine}" alt="Immagine Domanda" class="bnw">`;
+        } else {
+            timbroContainer.innerHTML = '';
+        }
+    }
+
+    if (currentIndex > 0) {
+        mostraDomandaCorrente();
+    } else {
+        document.querySelector(".pagina#pagina-benvenuto").classList.add("active");
+    }
+
+    aggiornaProgressBar();
+});
---
+++ script4.js
@@ -0,0 +1,141 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const domande = [
+        { domanda: "RECATI SUBITO ALLA TAPPA NUMERO 9. Benvenuti in PAKISTAN! Per superare questa tappa e fare tanti punti segui attentamente le indicazioni dello staff!", rispostaCorretta: "arrosto", immagine: "immagini/pakistan.svg" },
+        { domanda: "VAI ALLA TAPPA 10! Benvenuti IN ROMANIA! Leggi le istruzioni attentamente e avrai la parola d’ordine", rispostaCorretta: "tiramisu", immagine: "immagini/romania.svg" },
+        { domanda: "VOLA ALLA TAPPA 11! Benvenuti in SENEGAL! Sui tavolini troverai un cruciverba. Una volto risolto avrai la parola d’ordine!", rispostaCorretta: "meringata", immagine: "immagini/senegal.svg" },
+        { domanda: "VAI ALLA TAPPA 12. Benvenuti in TUNISIA! Ascolta bene lo staff!", rispostaCorretta: "pastiera", immagine: "immagini/tunisia.svg" },
+        { domanda: "VAI ALLA TAPPA 13. Benvenuti in UCRAINA! Ascolta bene lo staff!", rispostaCorretta: "cannoli", immagine: "immagini/ucraina.svg" },
+        { domanda: "VAI ALLA TAPPA 14. Benvenuti in SPAGNA! Ascolta lo staff per procedere", rispostaCorretta: "sorbetto", immagine: "immagini/spagna.svg" },
+        { domanda: "VIA LIBERA PER LA TAPPA 15! Benvenuti in BRASILE! Leggi le istruzioni attentamente e avrai la parola d’ordine", rispostaCorretta: "crostata", immagine: "immagini/brasile.svg" }
+    ];
+
+    const squadra = parseInt(document.body.getAttribute('data-squadra'), 7);
+    const domandeContainer = document.getElementById("domande-container");
+    const timbroContainer = document.getElementById("timbro-container");
+    const progressBar = document.getElementById("progress-bar");
+
+    function getDomandePerSquadra(domande, squadra) {
+        const start = (squadra - 1) % domande.length;
+        const orderedQuestions = [];
+
+        for (let i = 0; i < domande.length; i++) {
+            orderedQuestions.push(domande[(start + i) % domande.length]);
+        }
+
+        return orderedQuestions;
+    }
+
+    const domandePerSquadra = getDomandePerSquadra(domande, squadra);
+    let currentIndex = localStorage.getItem('currentIndex') ? parseInt(localStorage.getItem('currentIndex')) : 0;
+    let risposteCorrette = localStorage.getItem('risposteCorrette') ? parseInt(localStorage.getItem('risposteCorrette')) : 0;
+
+    domandePerSquadra.forEach((domandaObj, index) => {
+        const div = document.createElement("div");
+        div.className = "domanda";
+        div.innerHTML = `
+            <label for="domanda${index + 1}">${domandaObj.domanda}</label>
+            <div class="cici"></div>
+            <p>PAROLA D'ORDINE</p>
+            <input type="text" class="domandaDinamica" id="domanda${index + 1}" name="domanda${index + 1}">
+        `;
+        domandeContainer.insertBefore(div, document.getElementById("pagina-commiato"));
+    });
+
+    function aggiornaProgressBar() {
+        const progressPercent = (risposteCorrette / domandePerSquadra.length) * 100;
+        progressBar.style.width = `${progressPercent}%`;
+    }
+
+    function mostraProssimaDomanda() {
+        const activeDiv = document.querySelector(".domanda.active, .pagina.active");
+        const isPagina = activeDiv.classList.contains("pagina");
+
+        if (isPagina) {
+            activeDiv.classList.remove("active");
+            currentIndex++;
+            localStorage.setItem('currentIndex', currentIndex);
+            const nextDiv = document.querySelectorAll(".domanda, .pagina")[currentIndex];
+            nextDiv.classList.add("active");
+            if (nextDiv.classList.contains("domanda")) {
+                timbroContainer.innerHTML = `<img src="${domandePerSquadra[currentIndex - 1].immagine}" alt="Immagine Domanda" class="bnw">`;
+            } else {
+                timbroContainer.innerHTML = '';
+            }
+        } else {
+            const input = activeDiv.querySelector("input");
+            const risposta = input.value.trim();
+            const rispostaCorretta = domandePerSquadra[currentIndex - 1].rispostaCorretta;
+            const immagine = domandePerSquadra[currentIndex - 1].immagine;
+
+            if (risposta.toLowerCase() === rispostaCorretta.toLowerCase()) {
+                const imgElement = timbroContainer.querySelector('img');
+                imgElement.classList.remove('bnw');
+                imgElement.classList.add('color');
+
+                const timbro = document.createElement('div');
+                timbro.className = 'timbro';
+                timbroContainer.appendChild(timbro);
+
+                timbro.style.display = 'block';
+
+                risposteCorrette++;
+                localStorage.setItem('risposteCorrette', risposteCorrette);
+                aggiornaProgressBar();
+
+                setTimeout(() => {
+                    timbro.style.display = 'none';
+                    activeDiv.classList.remove("active");
+                    currentIndex++;
+                    localStorage.setItem('currentIndex', currentIndex);
+                    const nextDiv = document.querySelectorAll(".domanda, .pagina")[currentIndex];
+                    nextDiv.classList.add("active");
+                    if (nextDiv.classList.contains("domanda")) {
+                        nextDiv.querySelector("input").focus();
+                        timbroContainer.innerHTML = `<img src="${domandePerSquadra[currentIndex - 1].immagine}" alt="Immagine Domanda" class="bnw">`;
+                    } else {
+                        timbroContainer.innerHTML = '';
+                        document.getElementById("next-button").style.display = "none";
+                        localStorage.removeItem('currentIndex');
+                        localStorage.removeItem('risposteCorrette');
+                    }
+                }, 1000);
+            } else {
+                alert("Risposta errata. Riprova.");
+            }
+        }
+    }
+
+    document.getElementById("next-button").addEventListener("click", mostraProssimaDomanda);
+
+    document.querySelectorAll(".domanda input").forEach(input => {
+        input.addEventListener("keypress", function(event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                mostraProssimaDomanda();
+            }
+        });
+    });
+
+    function mostraDomandaCorrente() {
+        const divs = document.querySelectorAll(".domanda, .pagina");
+        divs.forEach(div => div.classList.remove("active"));
+
+        const activeDiv = divs[currentIndex];
+        activeDiv.classList.add("active");
+
+        if (activeDiv.classList.contains("domanda")) {
+            activeDiv.querySelector("input").focus();
+            timbroContainer.innerHTML = `<img src="${domandePerSquadra[currentIndex - 1].immagine}" alt="Immagine Domanda" class="bnw">`;
+        } else {
+            timbroContainer.innerHTML = '';
+        }
+    }
+
+    if (currentIndex > 0) {
+        mostraDomandaCorrente();
+    } else {
+        document.querySelector(".pagina#pagina-benvenuto").classList.add("active");
+    }
+
+    aggiornaProgressBar();
+});
